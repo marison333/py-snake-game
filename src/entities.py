087077787da29f2a80cdba901276499874b91156
marison333 <marison.sol@outlook.com>
2026-03-11 11:32:14 +0100

    feat: created new entities file for entity classes

---
+++ src/entities.py
@@ -0,0 +1,24 @@
+# class for anything that exist in the game
+class Entity:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+    def take_damage(self, amount):
+        self.health = self.health - amount
+
+    def attack(self, target):
+        target.take_damage(10)
+
+# child classes that makes use of the class Entity and then adds its own stuff
+# player has name, health and inventory
+class Player(Entity):
+    def __init__(self, name, health):
+        super().__init__(name, health)
+
+# monster has name, health and loot
+class Monster(Entity):
+    def __init__(self, name, health, loot):
+        super().__init__(name, health)
+        self.loot = loot
+
