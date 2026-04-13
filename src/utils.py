101e5e4a6d4a3306a9faf2a6c8fb995362640de8
riciano2004 <riciano2004@hotmail.com>
2026-04-13 17:10:14 +0200

    Ref: Updated printscreen to resolve errors

--- src/utils.py
+++ src/utils.py
@@ -15,7 +15,7 @@ def ask_choice(question, options):
 
 
 def print_screen(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
         print(file.read())
 
 
